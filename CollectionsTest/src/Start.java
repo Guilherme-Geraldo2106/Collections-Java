743a807545d05655ef7af21fe5fb1eb664775a45
Guilherme-Geraldo2106 <guilherme.geraldo2106@gmail.com>
2023-08-17 21:39:59 -0300

    ok

---
+++ CollectionsTest/src/Start.java
@@ -0,0 +1,11 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int numTest = 54;
+		System.out.print(numTest);
+	}
+	
+}
